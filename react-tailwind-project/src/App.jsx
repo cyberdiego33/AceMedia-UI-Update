479ed9575e6f6e2cfe3b81734a3e6cacd1365e59
Diego <iwujigoodluck5@gmail.com>
2025-05-06 14:19:34 -0700

    Update App.jsx

--- react-tailwind-project/src/App.jsx
+++ react-tailwind-project/src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import "./App.css";
 
 function NavBar() {
-  const [count, setCount] = useState(0);
 
   return <header className="sticky top-0 h-[200px] bg-white">
     <div>
